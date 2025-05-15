93e696244a5e0f3f19a41fea4d76b269fb38e2e6
Vapniak <113126917+Vapniak@users.noreply.github.com>
2025-05-15 18:49:37 +0200

    Basic folder structure

---
+++ CorebrainCS/Corebrain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace CorebrainCS
+{
+    public class CorebrainCS
+    {
+
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## How to download submodules?
 
 ``git submodule update --init``
+
+## How to run? 
+
+1. Download the submodules
+2. Create the venv environment
