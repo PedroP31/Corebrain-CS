1100705618d9e3dcff3e7b67e865de091e66fedd
Vapniak <113126917+Vapniak@users.noreply.github.com>
2025-05-16 09:16:56 +0200

    Working script

--- CorebrainCSTesting/Program.cs
+++ CorebrainCSTesting/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CorebrainCS;
+
+Console.WriteLine("Hello, World!");
+
+var corebrain = new CorebrainCS.CorebrainCS("../.venv/Scripts/python.exe", "../corebrain/corebrain/cli", false);
+
+Console.WriteLine(corebrain.Version());
