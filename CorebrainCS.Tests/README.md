3f582b242853bd4cacbc7fe400ef24f3f8390e6f
Oliwier Adamczyk <113126917+Vapniak@users.noreply.github.com>
2025-05-16 10:28:16 +0200

    Rename the corebrain tests

---
+++ CorebrainCS.Tests/README.md
@@ -0,0 +1,3 @@
+### Quick start
+
+* Create venv in the root directory and install all the dependencies. The instalation guide is in corebrain README.md
--- CorebrainCSTesting/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using CorebrainCS;
-
-Console.WriteLine("Hello, World!");
-
-var corebrain = new CorebrainCS.CorebrainCS("../.venv/Scripts/python.exe", "../corebrain/corebrain/cli", false);
-
-Console.WriteLine(corebrain.Version());
