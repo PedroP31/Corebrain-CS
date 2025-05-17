afaaa3fdd997dc1bb1095e8cf0b1a484747b38bf
Oliwier Adamczyk <113126917+Vapniak@users.noreply.github.com>
2025-05-17 17:24:45 +0200

    Add editorconfig and fix styling

--- CorebrainCS.Tests/Program.cs
+++ CorebrainCS.Tests/Program.cs
@@ -3,6 +3,6 @@ using CorebrainCS;
 Console.WriteLine("Hello, World!");
 
 // For now it only works on windows
-var corebrain = new CorebrainCS.CorebrainCS("../.venv/Scripts/python.exe", "../corebrain/corebrain/cli", false);
+var corebrain = new CorebrainCS.CorebrainCS("../corebrain/venv/Scripts/python.exe", "../corebrain/corebrain/cli", false);
 
 Console.WriteLine(corebrain.Version());
